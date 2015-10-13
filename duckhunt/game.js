5b46079770a3255f34fb1640472fc65889fc11e1
Supreme Master <bruno_olm94@wr-130-64-93-193.medford.tufts.edu>
2015-10-13 18:26:44 -0400

    fixing kinks in javascript

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,16 +1,15 @@
-function init(){
-	var canvas = document.getElementById('game_canvas');
-	var ctx = canvas.getContext('2d');
-	var img = new Image();
-
-	img.onload = function(){
-		ctx.drawImage(img, 0, 0);
-		var animals = new Image();
-		animals.onload = function() {
-			ctx.drawImage(animals, 40, 110, 40, 40, 80, 50, 50, 50);
-			ctx.drawImage(animals, 130, 110, 40, 40, 150, 20, 50, 50);
-		};
-		animals.src = 'duckhunt_various_sheet.png';
-	};
-	img.src = 'duckhunt-background.gif';
-};
+function init() {
+    var canvas = document.getElementById('game_canvas');
+    var ctx = canvas.getContext('2d');
+    var img = new Image();
+    img.onload = function() {
+        ctx.drawImage(img, 0, 0);
+        var animals = new Image();
+        animals.onload = function () {
+            ctx.drawImage(animals, 40, 110, 40, 40, 80, 50, 50, 50);
+            ctx.drawImage(animals, 130, 110, 40, 40, 150, 20, 50, 50);
+        };
+        animals.src = 'duckhunt_various_sheet.png';
+    };
+    img.src = 'duckhunt-background.gif';
+}
